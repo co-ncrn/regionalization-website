597f8a963c78b923409fe3c5a8ccc2734869a109
Owen Mundy <omundy@gmail.com>
2018-03-08 15:09:33 -0500

    Turn debugging back on, remote site keeps choosing new msa, why?

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $site = array(
     // data path
     'dataDir' => "/data/",
     // to debug or not
-    'debug' => 0,
+    'debug' => 1,
     // description: for meta tags, etc.
     'description' => "Reducing the Margin of Error in the American Community Survey",
     // domain for reference
